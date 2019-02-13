7bdb11f1d70d3152030dfb81119db2bd104ae364
Akshay Srinivas <08.akshaysrinivas@gmail.com>
2019-02-13 12:22:35 +0530

    Contracts created OK

--- contracts/HelpHandToken.sol
+++ contracts/HelpHandToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.5.0;
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -11,7 +11,7 @@ interface IERC20 {
   function transfer(address to, uint256 value) external returns (bool);
   function approve(address spender, uint256 value)
     external returns (bool);
-  function transferFrom(address from, address to, uint256 value)
+  function transferFrom(address payable from, address payable to, uint256 value)
     external returns (bool);
   event Transfer(
     address indexed from,
--- contracts/TokenFunc.sol
+++ contracts/TokenFunc.sol
@@ -1,10 +1,12 @@
-pragma solidity ^0.5.3;
-import "./erc20.sol";
-import "./ownable.sol";
+pragma solidity ^0.5.0;
+import "./HelpHandToken.sol";
+import "./Owner.sol";
+
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
  */
+
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
@@ -14,8 +16,9 @@ library SafeMath {
     assert(c / a == b);
     return c;
   }
+  
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    require(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
@@ -30,15 +33,18 @@ library SafeMath {
     return c;
   }
 }
-contract ERC20 is Ownable, IERC20 {
+
+contract ERC20 is Owner, IERC20 {
     
     using SafeMath for uint256;
     
     mapping(address => uint256) balances;
-  uint256 totalSupply_;
+    uint256 totalSupply_;
+  
   /**
   * @dev total number of tokens in existence
   */
+  
   function totalSupply() public view returns (uint256) {
     return totalSupply_;
   }
@@ -48,6 +54,7 @@ contract ERC20 is Ownable, IERC20 {
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
+  
   function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
@@ -63,24 +70,27 @@ contract ERC20 is Ownable, IERC20 {
   * @param _owner The address to query the the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
+  
   function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
   }
-mapping (address => mapping (address => uint256)) internal allowed;
+    mapping (address => mapping (address => uint256)) internal allowed;
+  
   /**
    * @dev Transfer tokens from one address to another
    * @param _from address The address which you want to send tokens from
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+   
+  function transferFrom(address payable _from, address payable _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-   emit Transfer(_from, _to, _value);
+    emit Transfer(_from, _to, _value);
     return true;
   }
   /**
@@ -95,18 +105,21 @@ mapping (address => mapping (address => uint256)) internal allowed;
    */
   function approve(address _spender, uint256 _value) public returns (bool) {
     allowed[msg.sender][_spender] = _value;
-  emit Approval(msg.sender, _spender, _value);
+    emit Approval(msg.sender, _spender, _value);
     return true;
   }
+  
   /**
    * @dev Function to check the amount of tokens that an owner allowed to a spender.
    * @param _owner address The address which owns the funds.
    * @param _spender address The address which will spend the funds.
    * @return A uint256 specifying the amount of tokens still available for the spender.
    */
+
   function allowance(address _owner, address _spender) public view returns (uint256) {
     return allowed[_owner][_spender];
   }
+  
   /**
    * @dev Increase the amount of tokens that an owner allowed to a spender.
    *
@@ -117,11 +130,13 @@ mapping (address => mapping (address => uint256)) internal allowed;
    * @param _spender The address which will spend the funds.
    * @param _addedValue The amount of tokens to increase the allowance by.
    */
+   
   function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
     allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
     emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
     return true;
   }
+  
   /**
    * @dev Decrease the amount of tokens that an owner allowed to a spender.
    *
@@ -132,6 +147,7 @@ mapping (address => mapping (address => uint256)) internal allowed;
    * @param _spender The address which will spend the funds.
    * @param _subtractedValue The amount of tokens to decrease the allowance by.
    */
+   
   function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
     uint oldValue = allowed[msg.sender][_spender];
     if (_subtractedValue > oldValue) {
@@ -143,24 +159,17 @@ mapping (address => mapping (address => uint256)) internal allowed;
     return true;
   }
 }
+
 contract Token is ERC20  {
-    string public constant name = 'TollCoin';
-    string public constant symbol = 'SMC';
+    string public constant name = 'HelpHandCoin';
+    string public constant symbol = 'HHC';
     uint8 public constant decimals = 6;
-    constructor()
-        public
-        payable
+    constructor() public payable    {
         
-    {
-        
-                uint premintAmount = 50*10**uint(decimals);
+                uint premintAmount = 31*10**uint(decimals);
                 totalSupply_ = totalSupply_.add(premintAmount);
                 balances[msg.sender] = balances[msg.sender].add(premintAmount);
-              emit  Transfer(address(0), msg.sender, premintAmount);
-            
-        
-        //address(0x30CdBB020BFc407d31c5E5f4a9e7fC3cB89B8956).transfer(100000000000000000 wei);
-        //address(0x30CdBB020BFc407d31c5E5f4a9e7fC3cB89B8956).transfer(400000000000000000 wei);
+                emit Transfer(address(0), msg.sender, premintAmount);
             
     }
 }
